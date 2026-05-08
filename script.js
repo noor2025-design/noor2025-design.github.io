fb4d2a02aa1b41d7c746706ff633efd7baf8b475
noor2025-design <abdun221@newschool.edu>
2026-05-08 17:15:44 -0400

    worked on about me section

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-let introHeading = document.querySelector(".intro-section h1")
-let introHeadingText = introHeading.textContent
+// let introHeading = document.querySelector(".intro-section h1")
+// let introHeadingText = introHeading.textContent
 
-let spanText = ""
+// let spanText = ""
 
-let introHeadingArray = introHeadingText.split("")
-console.log(introHeadingArray);
+// let introHeadingArray = introHeadingText.split("")
+// console.log(introHeadingArray);
 // splits a string into an array 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
-introHeadingArray.forEach((character)=>{
-let spanElement = `<span class="animate-translate"><span class="animate-grow">${character}</span></span>`    
-spanText += spanElement
-})
-introHeading.innerHTML = spanText
+// introHeadingArray.forEach((character)=>{
+// let spanElement = `<span class="animate-translate"><span class="animate-grow">${character}</span></span>`    
+// spanText += spanElement
+// })
+// introHeading.innerHTML = spanText
 // we loop through the array and add each template to the span text using +=
-let allSpanElements = document.querySelectorAll("h1 > span")
-allSpanElements.forEach((element)=>{
+// let allSpanElements = document.querySelectorAll("h1 > span")
+// allSpanElements.forEach((element)=>{
     // creates a random number between 0-.5
-    let offset = Math.random()*1
-    element.style.animationDelay = `${offset}s`
-    element.querySelector("span").style.animationDelay = `${offset}s`
-})
+//     let offset = Math.random()*1
+//     element.style.animationDelay = `${offset}s`
+//     element.querySelector("span").style.animationDelay = `${offset}s`
+// })
 
 let svgElement = document.getElementById("page-load-svg")
 let circleSvg = document.getElementById("svg-circle-one")
