57e1cb113c8d211fb5e7c421840f6f551b07131b
noor2025-design <abdun221@newschool.edu>
2026-05-08 08:03:55 -0400

    updates to trying out name animations

--- script.js
+++ script.js
@@ -14,9 +14,10 @@ spanText += spanElement
 })
 introHeading.innerHTML = spanText
 // we loop through the array and add each template to the span text using +=
-let allSpanElements = document.querySelectorAll("h1 span")
+let allSpanElements = document.querySelectorAll("h1 > span")
 allSpanElements.forEach((element)=>{
     // creates a random number between 0-.5
     let offset = Math.random()*1
     element.style.animationDelay = `${offset}s`
+    element.querySelector("span").style.animationDelay = `${offset}s`
 })
